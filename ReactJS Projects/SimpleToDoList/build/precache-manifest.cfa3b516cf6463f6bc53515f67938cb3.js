6792ee37d0c864e94520048e68f217f30999707e
Ian Key <ian@iankey.co.uk>
2022-08-14 18:51:46 +0100

    Update

---
+++ ReactJS Projects/SimpleToDoList/build/precache-manifest.cfa3b516cf6463f6bc53515f67938cb3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a031f6fe0f0175b1d15",
+    "url": "/static/js/main.0a031f6f.chunk.js"
+  },
+  {
+    "revision": "3a5d51bcc8b5a5cbfd8f",
+    "url": "/static/js/1.3a5d51bc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0457c5bd6e0b25434f3fded30931b5df",
+    "url": "/index.html"
+  }
+];
